ae835c0a4e74a07ffe341f4c79d45d515f3e04e7
Koke Thiele <kokepizzadev@gmail.com>
2025-05-14 15:51:30 +0200

    array of projects in projects.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,9 +5,17 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 // ANIMACIÓ BLUR TITOL
-window.addEventListener("wheel", () => {
+let blurTimeout;
+function handleBlur() {
     const MARC = document.querySelector(".title-name");
     if(MARC) {
         MARC.style.filter = "blur(0px)";
+        clearTimeout(blurTimeout);
+        blurTimeout = setTimeout(() => {
+            MARC.style.filter = "";
+        }, 100);
     }
-});
+}
+window.addEventListener("wheel", handleBlur);
+window.addEventListener("scroll", handleBlur);
+window.addEventListener("touchmove", handleBlur);
---
+++ src/assets/projects.js
@@ -0,0 +1,60 @@
+// IMATGES
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.jpg';
+import img6 from '../assets/6.jpg';
+import img7 from '../assets/7.jpg';
+import img8 from '../assets/8.jpg';
+
+export const projects = [
+    {
+        url: "/project-1",
+        name: "Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img1, img2, img3, img4, img5, img6, img7, img8]
+    },
+    {
+        url: "/project-2",
+        name: "Project 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img2, img3, img4, img5, img6, img7, img8, img1]
+    },
+    {
+        url: "/project-3",
+        name: "Project 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img3, img4, img5, img6, img7, img8, img1, img2]
+    },
+    {
+        url: "/project-4",
+        name: "Project 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img4, img5, img6, img7, img8, img1, img2, img3]
+    },
+    {
+        url: "/project-5",
+        name: "Project 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img5, img6, img7, img8, img1, img2, img3, img4]
+    },
+    {
+        url: "/project-6",
+        name: "Project 6",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img6, img7, img8, img1, img2, img3, img4, img5]
+    },
+    {
+        url: "/project-7",
+        name: "Project 7",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img7, img8, img1, img2, img3, img4, img5, img6]
+    },
+    {
+        url: "/project-8",
+        name: "Project 8",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget aliquet.",
+        images: [img8, img1, img2, img3, img4, img5, img6, img7]
+    }
+]
