27ed858fcceac5069451f0ab6743840a9f0e30a3
Koke Thiele <kokepizzadev@gmail.com>
2025-06-20 14:52:05 +0200

    Refactor Gallery and GalleryWrapper components for improved functionality and styling; remove unused gallery.css

--- src/components/GalleryWrapper.tsx
+++ src/components/GalleryWrapper.tsx
@@ -4,7 +4,8 @@ const GalleryWrapper = () => {
   useEffect(() => {
     const wrapper = document.querySelector(".projects-wrapper");
     const cards = document.querySelectorAll(".project-card");
-    const gallery = document.querySelector(".gallery") as HTMLElement;
+    const gallery = document.getElementById("gallery") as HTMLElement;
+    const titleLink = document.getElementById("project-link") as HTMLAnchorElement;
     const markX = window.innerWidth / 2;
 
     const updateCard = () => {
@@ -17,32 +18,45 @@ const GalleryWrapper = () => {
 
         const isOver = markX >= left && markX <= right;
 
-        if (isOver && !found && gallery) {
-        card.classList.add("active");
-        const imageURL = card.getAttribute("data-image");
-        if (imageURL) {
-            gallery.style.backgroundImage = `url('${imageURL}')`;
-        }
-        found = true;
+        if (isOver && !found && gallery && titleLink) {
+          card.classList.add("active");
+
+          const imageURL = card.getAttribute("data-image");
+          const name = card.getAttribute("data-name");
+          const slug = card.getAttribute("data-slug");
+
+          if (imageURL) gallery.style.backgroundImage = `url('${imageURL}')`;
+          if (name && slug) {
+            titleLink.href = slug;
+            titleLink.innerHTML = `<h2>${name} ↗</h2>`;
+          }
+
+          found = true;
         } else {
           card.classList.remove("active");
         }
       });
     };
 
-    if (wrapper) {
+    const scrollToEnd = () => {
+      if (wrapper) {
         wrapper.scrollTo({
-            left: wrapper.scrollWidth,
-            behavior: "auto"
+          left: wrapper.scrollWidth,
+          behavior: "auto",
         });
-    }
+      }
+    };
+
+    setTimeout(() => {
+      scrollToEnd();
+      updateCard();
+    }, 100);
 
     wrapper?.addEventListener("scroll", () => {
       requestAnimationFrame(updateCard);
     });
 
     window.addEventListener("load", updateCard);
-    updateCard();
 
     return () => {
       wrapper?.removeEventListener("scroll", updateCard);
